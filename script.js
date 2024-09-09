38ce3797921793421604d6be63ac678553774107
Magestick <74025726+Magest1ckkz@users.noreply.github.com>
2024-09-09 20:15:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+    const input = document.querySelector('input');
+		const output = document.querySelector('#output');
+
+		input.addEventListener('keypress', (event) => {
+			if (event.key === 'Enter') {
+				const value = input.value.trim();
+				switch (value) {
+
+					case 'test':
+						output.innerHTML = '<p>test</p>';
+						break;
+
+					 case 'help':
+                        output.innerHTML = '<p>MageTerminal Help Page (0.0.4)<br><br>help - Shows this message.<br>test - A testing string.<br>date - Output the current date and time.</p>';
+					    break;
+
+                    case 'date':
+                    const now = new Date();
+                let formattedDate;
+                if (now.getDate() < 10) {
+                    formattedDate = `0${now.getDate()}-`;
+                } else {
+                    formattedDate = `${now.getDate()}-`;
+                }
+                if (now.getMonth() + 1 < 10) {
+                    formattedDate += `0${now.getMonth() + 1}-`;
+                } else {
+                    formattedDate += `${now.getMonth() + 1}-`;
+                }
+                formattedDate += now.getFullYear();
+                const hours = now.getHours().toString().padStart(2, '0');
+                const minutes = now.getMinutes().toString().padStart(2, '0');
+                const seconds = now.getSeconds().toString().padStart(2, '0');
+                output.innerHTML = `<p>${formattedDate} ${hours}:${minutes}:${seconds}</p>`;
+                break;
+
+					default:
+						output.innerHTML = `Invalid command: ${input.value}. Type "help" to show all commands.`;
+						break;
+				}
+				input.value = '';
+			}
+		});
